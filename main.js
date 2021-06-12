9826a20ff0c29198bfd1cae9e5f8bdf6f1fcb00d
AzamatOrtiqov <azamatortiqov77@gmail.com>
2021-06-12 15:49:22 +0500

    Update files

--- main.js
+++ main.js
@@ -6,12 +6,13 @@ window.navigator.geolocation.getCurrentPosition(async success => {
     const lat = success.coords.latitude
     const long = success.coords.longitude
     const tBodyElement = document.querySelector(".tbody")
+    const cityElement = document.querySelector(".city");
 
     let responce = await fetch(`https://api.aladhan.com/v1/timings/calendar?latitude=${lat}&longitude=${long}&method=3&school=1&month=${new Date().getMonth()}&year=${new Date().getFullYear()}`)
 
     responce = await responce.json();
 
-    console.log(responce);
+    cityElement.textContent = `(${responce.data.meta.timezone})`
 
     for (let data in responce.data.timings) {
         // console.log(data , responce.data.timings[data])
