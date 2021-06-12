2af768932df2d4c2267cca45fe69fc7846ddff08
AzamatOrtiqov <azamatortiqov77@gmail.com>
2021-06-12 15:35:16 +0500

    Add files

---
+++ main.js
@@ -0,0 +1,40 @@
+alert("Double click the screen to know the prayer times.");
+const wrapperElement = document.querySelector("#wrapper");
+
+window.navigator.geolocation.getCurrentPosition(async success => {
+
+    const lat = success.coords.latitude
+    const long = success.coords.longitude
+    const tBodyElement = document.querySelector(".tbody")
+
+    let responce = await fetch(`https://api.aladhan.com/v1/timings/calendar?latitude=${lat}&longitude=${long}&method=3&school=1&month=${new Date().getMonth()}&year=${new Date().getFullYear()}`)
+
+    responce = await responce.json();
+
+    console.log(responce);
+
+    for (let data in responce.data.timings) {
+        // console.log(data , responce.data.timings[data])
+        const trElement = document.createElement("tr")
+        const tdNameElement = document.createElement("td")
+        const tdTimeElement = document.createElement("td")
+        tdNameElement.classList.add("fw-bold")
+        tdTimeElement.classList.add("fw-bold")
+
+        tdNameElement.textContent = data;
+        tdTimeElement.textContent = responce.data.timings[data];
+
+        trElement.appendChild(tdNameElement);
+        trElement.appendChild(tdTimeElement);
+
+        tBodyElement.appendChild(trElement);
+
+    }
+}, error => {
+    alert("You don't give permission to get location")
+})
+
+window.addEventListener("dblclick" , event => {
+    wrapperElement.classList.remove("d-none")
+})
+
